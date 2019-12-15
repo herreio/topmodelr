f63f1663715300de7abeb68008c7d992094006e6
herreio <git@herre.io>
2019-12-15 15:22:14 +0100

    R/corpus_pre.R updated

--- R/corpus_pre.R
+++ R/corpus_pre.R
@@ -3,8 +3,8 @@
 # ---------------------- #
 
 #' @export
-corpus_stop <- function() {
-    stopwords::stopwords("english", source="smart")
+corpus_create <- function(char_vec) {
+    tm::VCorpus(tm::VectorSource(char_vec))
 }
 
 #' @export
@@ -14,19 +14,25 @@ corpus_prep <- function(corp) {
     corp <- tm::tm_map(corp, tm::content_transformer(lnbr))
     corp <- tm::tm_map(corp, tm::content_transformer(tags))
     corp <- tm::tm_map(corp, tm::content_transformer(nums))
-    corp <- tm::tm_map(corp, tm::content_transformer(pnct))
     corp <- tm::tm_map(corp, tm::content_transformer(cntrl))
+    corp <- tm::tm_map(corp, tm::content_transformer(ascii))
+    corp <- tm::tm_map(corp, tm::content_transformer(pnct))
     corp <- tm::tm_map(corp, tm::content_transformer(tolower))
     corp <- tm::tm_map(corp, function(x) { tm::removeWords(x, corpus_stop()) })
     corp <- tm::tm_map(corp, tm::content_transformer(trim))
     tm::tm_map(corp, tm::content_transformer(trim2))
 }
 
-# returns string w/o leading or trailing whitespace
-trim2 <- function (x) gsub("^\\s+|\\s+$", "", x)
+#' @export
+corpus_stop <- function() {
+    stopwords::stopwords("english", source="smart")
+}
 
 # returns string w/o multiple whitespaces
 trim <- function (x) gsub("\\s+", " ", x)
+
+# returns string w/o leading or trailing whitespace
+trim2 <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 # returns string w/o line breaks
 lnbr <- function(x) gsub("[\r\n]", " ", x)
@@ -35,7 +41,10 @@ lnbr <- function(x) gsub("[\r\n]", " ", x)
 tags <- function (x) gsub("<.*?>", "", x)
 
 # returns string w/o multiple whitespaces
-nums <- function (x) gsub("[[:digit:]]", "", x)
+nums <- function (x) gsub("[^ ]*[[:digit:]][^ ]*", "", x, perl=T)
+
+# remove words containing non-ascii characters
+ascii <- function(x) gsub("[^ ]*[^\\x00-\\x7F][^ ]*", "", x, perl=T)
 
 # remove punction marks
 pnct <- function (x) gsub("[[:punct:]]"," ", x)
